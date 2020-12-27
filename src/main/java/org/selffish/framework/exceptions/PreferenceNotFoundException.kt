3ca6630fd4e1b8e378a24a7225eec5bf8194067d
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-27 15:27:37 +0100

    Adding missing exception

---
+++ src/main/java/org/selffish/framework/exceptions/PreferenceNotFoundException.kt
@@ -0,0 +1,8 @@
+package org.selffish.framework.exceptions
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+import java.lang.RuntimeException
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "preference not found")
+class PreferenceNotFoundException: RuntimeException()
